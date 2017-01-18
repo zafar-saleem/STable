cf200a44c2cc5cb2f38cf7b897bdad8c7a0c421c
Zafar Saleem <zafar-saleem@users.noreply.github.com>
2017-01-18 12:01:32 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import CTable from '../dist/Stable';
-/*
 import STableModel from './scripts/models/STableModel';
 import STableView from './scripts/views/STableView';
 
@@ -7,5 +5,3 @@ STableView.init(STableModel, {
     el: document.querySelector('.wrapper'),
     url: 'src/datasets/example-one.json'
 });
-*/
-
